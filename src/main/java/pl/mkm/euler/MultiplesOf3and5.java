5eb2da3628cb9938c67aa5af30e7ec6e756641c4
MKMichalski <mateuszmichalskiK@gmail.com>
2018-06-23 15:24:31 +0200

    problem 1 solved: Multiple of 3 and 5

--- src/main/java/pl/mkm/euler/MultiplesOf3and5.java
+++ src/main/java/pl/mkm/euler/MultiplesOf3and5.java
@@ -2,7 +2,10 @@ package pl.mkm.euler;
 
 public class MultiplesOf3and5 {
     public static void main(String[] args) {
-
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) sum = sum + i;
+        }
+        System.out.println(sum);
     }
-
 }
